1100196ed952833d9cc6b70d20d7040af762d0d5
Subhro Majumder <48013811+msubhro@users.noreply.github.com>
2020-06-17 11:58:09 +0530

    Add files via upload

---
+++ LocateComputerInForest/GetComputerDetails V 1.0.ps1
@@ -0,0 +1,25 @@
+﻿Write-Host "  "
+ 
+$inputpath= Read-host "Enter the path of the input file. It shoudl be a TXT file."
+ 
+Write-Host "  "
+
+$outputpath= Read-host "Enter the path of the output file. It shoudl be a CSV file."
+
+Write-Host "  "
+
+$ErrorActionPreference = 'silentlycontinue '
+
+$domains = (Get-ADForest).Domains
+
+foreach ($domain in $domains)
+
+
+{
+
+$output= Get-Content -Path $inputpath | Get-ADComputer -Server $domain -Properties * | Select-Object Name,DistinguishedName,DNSHostName,OperatingSystem,IPv4Address,CanonicalName
+
+$output | ft *
+
+$output | export-csv -Path $outputpath -NoTypeInformation -Append
+}
